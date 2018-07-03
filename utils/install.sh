339eca0788e61008cb1e571d45ed689609a2f568
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-07-03 00:09:12 -0300

    Melhorias em script de instalação

--- utils/install.sh
+++ utils/install.sh
@@ -12,28 +12,35 @@
 #                  If no version is specified, the most recent one is fetched and installed
 
 declare VERSION="$1"
+declare -r RED='\033[0;31m'
 
 setVersion() {
-    if [ -z "$VERSION"]; then
+    if [ -z "$VERSION" ]; then
         # Kudos to Lukechilds and peschu123 for this nice line! (https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c)
         VERSION=$(curl --silent "https://api.github.com/repos/Miguel-Fontes/java-tooling-template/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
     fi
 
-    echo "Version $VERSION will be installed..."
+    echo -e "Version $VERSION will be installed..."
 }
 
 execute() {
 
     # Downloads the selected version from github releases
-    echo "Downloading selected version from github releases"
+    echo -e "Downloading selected version from github releases...\n"
     wget https://github.com/Miguel-Fontes/java-tooling-template/releases/download/java-tooling-template-"$VERSION"/java-tooling-template-"$VERSION".tar.gz
+
+    if [ ! -f "java-tooling-template-$VERSION.tar.gz" ]; then
+        echo -e "${RED}An error ocurred downloading java-tooling-template-"$VERSION".tar.gz."
+        echo -e "That version really exists? Check on https://github.com/Miguel-Fontes/java-tooling-template/releases!"
+        exit 1
+    fi
 
     # Uncompress the tar on current directory
     echo "Uncompressing tar.gz"
     tar -xvzf java-tooling-template-"$VERSION".tar.gz
 
     # Installs the jar on .m2
-    echo "Intalling artifact to .m2"
+    echo -e "Intalling artifact to .m2\n"
     file=$(find . -name '*.jar' -printf "%f\n")
     mvn install:install-file                                  \
       -Dfile="$file"                                          \
@@ -43,10 +50,10 @@ execute() {
       -Dversion=$VERSION
 
     # Cleanup
-    echo "Cleaning up..."
+    echo -e "Cleaning up..."
     rm "$file"
 
-    echo "Done!"
+    echo -e "Done!"
 }
 
 # execute
